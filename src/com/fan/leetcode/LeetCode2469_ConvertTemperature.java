7c04eb08715a1f8f2b871444de2c30e1818564ac
fan <1009976636@qq.com>
2023-03-21 13:19:44 +0800

    20230321

---
+++ src/com/fan/leetcode/LeetCode2469_ConvertTemperature.java
@@ -0,0 +1,10 @@
+package com.fan.leetcode;
+
+public class LeetCode2469_ConvertTemperature {
+    public double[] convertTemperature(double celsius) {
+        double[] res = new double[2];
+        res[0] = celsius + 273.15;
+        res[1] = celsius * 1.80 + 32.00;
+        return res;
+    }
+}
