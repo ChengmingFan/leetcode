e2e3a68aed9e7a8d4e1e2b5d68f030fdbfc92a34
chengming.fan <chengming.fan@thoughtworks.com>
2023-03-22 15:00:56 +0800

    not commit

--- src/com/fan/leetcode/UnicodeUtil.java
+++ src/com/fan/leetcode/UnicodeUtil.java
@@ -5,14 +5,23 @@ package com.fan.leetcode;
  */
 public class UnicodeUtil {
     public static void main(String[] args) {
-        System.out.println(unicodeToString("\\u5339\\u914d\\u5931\\u8d25"));
+        System.out.println(unicodeToString(""));
     }
     public static String unicodeToString(String unicode) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         String[] hex = unicode.split("\\\\u");
         for (int i = 1; i < hex.length; i++) {
             int index = Integer.parseInt(hex[i], 16);
             sb.append((char) index);
+        }
+        return sb.toString();
+    }
+
+    public static String stringToUnicode(String str) {
+        StringBuilder sb = new StringBuilder();
+        char[] c = str.toCharArray();
+        for (int i = 0; i < c.length; i++) {
+            sb.append("\\u").append(Integer.toHexString(c[i]));
         }
         return sb.toString();
     }
