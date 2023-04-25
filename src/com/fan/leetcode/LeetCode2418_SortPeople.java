256abed26f6104c09b4b223394699a17b5d902db
chengming.fan <chengming.fan@thoughtworks.com>
2023-04-25 10:27:17 +0800

    20230425

---
+++ src/com/fan/leetcode/LeetCode2418_SortPeople.java
@@ -0,0 +1,23 @@
+package com.fan.leetcode;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+/**
+ * Created by Chengming Fan on 2023/4/25 09:55
+ */
+public class LeetCode2418_SortPeople {
+    public String[] sortPeople(String[] names, int[] heights) {
+//        Map<Integer, String> map = new HashMap<>();
+//        for (int i = 0; i < names.length; i++) {
+//            map.put(heights[i], names[i]);
+//        }
+//        return Arrays.stream(heights).boxed().sorted(Comparator.reverseOrder()).map(map::get).toArray(String[]::new);
+        TreeMap<Integer, String> map = new TreeMap<>(Comparator.comparingInt(Integer::intValue).reversed());
+        for (int i = 0; i < names.length; i++) {
+            map.put(heights[i], names[i]);
+        }
+        return map.values().toArray(new String[0]);
+    }
+
+}
